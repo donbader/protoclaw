9fdfa24b4b397f501d3bc15907b947959c8de6e8
Corey Feng <corey.feng@fazzfinancial.com>
2026-04-05 06:38:02 +0800

    test(22-02): add SDK channel integration test

--- tests/integration/src/lib.rs
+++ tests/integration/src/lib.rs
@@ -1,5 +1,5 @@
 pub use protoclaw_test_helpers::{
     boot_supervisor_with_port, debug_http_path, make_handle, mock_agent_config,
-    mock_agent_config_with_options, mock_agent_path, wait_for_port, with_timeout, SseCollector,
-    SseEvent,
+    mock_agent_config_with_options, mock_agent_path, sdk_channel_config, sdk_tool_config,
+    wait_for_port, with_timeout, SseCollector, SseEvent,
 };
